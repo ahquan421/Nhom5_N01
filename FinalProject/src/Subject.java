1340f52fad4f4571a29e601d18f8886539756b33
Thuy Linh <23010633@st.phenikaa-uni.edu.vn>
2024-10-10 08:11:12 +0700

    update

--- FinalProject/src/Subject.java
+++ FinalProject/src/Subject.java
@@ -1,7 +1,54 @@
+import java.time.LocalDate;
+
 public class Subject {
     private String monhocID;
     private String tenmonhoc;
     private int sotc;
-    private int ngaybatdau;
-    private int ngayketthuc;
+    private LocalDate ngaybatdau;
+    private LocalDate ngayketthuc;
+
+    public String getMonhocID(){
+        return monhocID;
+    }
+    public void setMonhocID(String monhocID){
+        this.monhocID = monhocID;
+    }
+    public String getTenonhoc(){
+        return tenmonhoc;
+    }
+    public void setTenonhoc(String tenmonhoc){
+        this.tenmonhoc = tenmonhoc;
+    }
+    public int getSotc(){
+        return sotc;
+    }
+    public void setSotc(int sotc){
+        this.sotc = sotc;
+    }
+    public LocalDate getNgaybatdau() {
+        return ngaybatdau;
+    }
+    public void setNgaybatdau(LocalDate ngaybatdau) {
+        this.ngaybatdau = ngaybatdau;
+    }
+    public LocalDate getNgayketthuc() {
+        return ngayketthuc;
+    }
+    public void setNgayketthuc(LocalDate ngayketthuc) {
+        this.ngayketthuc = ngayketthuc;
+    }
+
+    public Subject(String monhocID, String tenmonhoc, int sotc, LocalDate ngaybatdau, LocalDate ngayketthuc) {
+        super();
+        this.monhocID = monhocID;
+        this.tenmonhoc = tenmonhoc;
+        this.sotc = sotc;
+        this.ngaybatdau = ngaybatdau;
+        this.ngayketthuc = ngayketthuc;
+    }
+
+    @Override
+    public String toString(){
+        return "Subject [monhocID=" + monhocID +", tenmonhoc=" + tenmonhoc + ", sotc=" + sotc + ", ngaybatdau=" + ngaybatdau + ", ngayketthuc=" + ngayketthuc + "]";
+    }
 }
