e456d2e7d46ae8bb2e8b0a423a94fae6fb772fff
Thuy Linh <23010633@st.phenikaa-uni.edu.vn>
2024-10-17 19:20:56 +0700

    update

--- FinalProject/src/Views/LoginSystem.java
+++ FinalProject/src/Views/LoginSystem.java
@@ -1,4 +1,6 @@
+package Views;
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
--- FinalProject/src/Views/SubjectManagementSystem.java
+++ FinalProject/src/Views/SubjectManagementSystem.java
@@ -1,5 +1,7 @@
+package Views;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
